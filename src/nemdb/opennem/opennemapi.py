ec348962468dab4034b8a73a9cb035ba59602185
Youssef Miftah <miftahyo@outlook.fr>
2025-09-21 16:47:18 +1000

    opennemm api wip

---
+++ src/nemdb/opennem/opennemapi.py
@@ -0,0 +1,101 @@
+from joblib import Memory
+import datetime
+
+import polars as pl
+
+from openelectricity import AsyncOEClient
+from openelectricity.types import (
+    DataInterval,
+    NetworkCode,
+    DataMetric,
+    DataPrimaryGrouping,
+    DataSecondaryGrouping,
+)
+
+from nemdb.config import Config
+
+memory = Memory(location=Config.TEMP_DIR, verbose=1)
+
+
+async def read_data(
+    metric: DataMetric | str,
+    network_code: NetworkCode = "NEM",
+    interval: DataInterval = "1h",
+    date_start: datetime.datetime | None = None,
+    date_end: datetime.datetime | None = None,
+    primary_grouping: DataPrimaryGrouping = "network_region",
+    secondary_grouping: DataSecondaryGrouping = "fueltech",
+) -> pl.DataFrame:
+    if isinstance(metric, str):
+        metric = DataMetric(metric)
+    if date_start is None and date_end is None:
+        today = datetime.datetime.today()
+        date_start = today - datetime.timedelta(days=2)
+        date_end = today
+    response = await _fetch_api(
+        metric=metric,
+        network_code=network_code,
+        interval=interval,
+        date_start=date_start,
+        date_end=date_end,
+        primary_grouping=primary_grouping,
+        secondary_grouping=secondary_grouping,
+    )
+    return _response_to_df(response)
+
+
+@memory.cache
+async def _fetch_api(
+    metric: DataMetric,
+    network_code: NetworkCode,
+    interval: DataInterval,
+    date_start: datetime.datetime,
+    date_end: datetime.datetime,
+    primary_grouping: DataPrimaryGrouping = "network_region",
+    secondary_grouping: DataSecondaryGrouping = "fueltech",
+):
+    async with AsyncOEClient() as client:
+        # Make async API calls
+        response = await client.get_network_data(
+            network_code=network_code,
+            metrics=[metric],
+            interval=interval,
+            date_start=date_start,
+            date_end=date_end,
+            primary_grouping=primary_grouping,
+            secondary_grouping=secondary_grouping,
+        )
+    return response
+
+
+def _response_to_df(response):
+    data = []
+    for timeseries in response.data:
+        for result in timeseries.results:
+            for data_point in result.data:
+                data.append(
+                    {
+                        "timestamp": data_point.timestamp,
+                        "metric": timeseries.metric,
+                        "value": data_point.value,
+                        "unit": timeseries.unit,
+                        "type": result.name,
+                    }
+                )
+    # Same data structure as above
+    return (
+        pl.DataFrame(data)
+        .with_columns(
+            pl.col("type")
+            .str.splitn("|", 2)
+            .struct.rename_fields(["region", "tech"])
+            .struct.unnest()
+        )
+        .with_columns(
+            pl.col("region")
+            .str.splitn("_", 2)
+            .struct.rename_fields(["metric", "region"])
+            .struct.unnest()
+        )
+        .drop("type")
+    )
